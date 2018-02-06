f45dce2ecdfde8c42fb3b3184d5752ae72f44da6
Ryan Mcpherson <mmcpher2@gmail.com>
2018-02-06 13:43:14 -0600

    fixed dotnet tool version, 2 tests passing

--- test/Bangazon.Tests/Bangazon_CustomerManagerShould.cs
+++ test/Bangazon.Tests/Bangazon_CustomerManagerShould.cs
@@ -1,19 +1,47 @@
 using System;
 using Xunit;
+using Bangazon;
+using System.Collections.Generic;
 
 namespace Bangazon.Tests
 {
     public class CustomerManagerShould
     {
+        private Customer _customer;
+
+        public CustomerManagerShould()
+        {
+            // properties of customer for hard coding.
+
+            _customer = new Customer(
+                1,
+                "Sam Phillips",
+                "1 Main St",
+                "Nashville",
+                "TN",
+                "37221",
+                "6157865437"
+            );
+        }
         [Fact]
         public void AddNewCustomer()
         {
-
+            Assert.Equal(_customer.Id, 1);
         }
 
         [Fact]
         public void ListCustomers()
         {
+
+        }
+
+        public void GetSingleCustomer()
+        {
+            CustomerManager manager = new CustomerManager();
+            manager.Add(_customer);
+            Customer customer = manager.GetSingleCustomer(1);
+
+            Assert.Equal(customer.Id, 1);
 
         }
     }
