741d4cf3a78812fb4f0c027eead7040af5ddca55
Ryan Mcpherson <mmcpher2@gmail.com>
2018-02-06 13:44:41 -0600

    version issue fixed, 2 tests passing

---
+++ src/Bangazon/Models/Customer.cs
@@ -0,0 +1,25 @@
+namespace Bangazon
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Phone { get; set; }
+
+        // Constructor for Customer
+        public Customer(int id, string name, string address, string city, string state, string postalCode, string phone)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Address = address;
+            this.City = city;
+            this.State = state;
+            this.PostalCode = postalCode;
+            this.Phone = phone;
+        }
+    }
+}
