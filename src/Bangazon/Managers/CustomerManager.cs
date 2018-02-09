3b3649ef9a61c1a13ab87908c4d3db122237b3fb
Ryan Mcpherson <mmcpher2@gmail.com>
2018-02-09 16:14:10 -0600

    Pushing from CLI directory

--- src/Bangazon/Managers/CustomerManager.cs
+++ src/Bangazon/Managers/CustomerManager.cs
@@ -27,9 +27,22 @@ namespace Bangazon.Managers
             return id;
         }
 
-        public Customer GetSingleCustomer (int Id)
+        public int GetSingleCustomer (int customerId)
         {
-            return _customerTable.Where(c => c.Id == Id).Single();
+            // return _customerTable.Where(c => c.Id == Id).Single();
+            Customer singleCustomer = new Customer();
+            _db.Query($"SELECT * FROM Customer WHERE Customer.CustomerId = {customerId};", (SqliteDataReader reader) => {
+                while (reader.Read()){
+                singleCustomer.Id = reader.GetInt32(0);
+                singleCustomer.Name = reader[1].ToString();
+                singleCustomer.Address = reader[2].ToString();
+                singleCustomer.City = reader[3].ToString();
+                singleCustomer.State = reader[4].ToString();
+                singleCustomer.PostalCode = reader[5].ToString();
+                singleCustomer.Phone = reader[6].ToString();
+                }
+            });
+            return singleCustomer.Id;
         }
 
         public List<Customer> ListCustomers ()
@@ -40,7 +53,7 @@ namespace Bangazon.Managers
         // Author: Leah Duvic
         // Purpose: setting the customer to active customer once selected.
 
-        public Customer ActiveCustomer (int Id)
+        public int ActiveCustomer (int Id)
         {
             var activeCustomer = GetSingleCustomer(Id);
             return activeCustomer;
