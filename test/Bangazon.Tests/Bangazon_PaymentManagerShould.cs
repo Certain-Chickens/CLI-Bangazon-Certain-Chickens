54f131abab5fb804486979f94839d9ecc05b1a5a
Ryan Mcpherson <mmcpher2@gmail.com>
2018-02-12 14:48:27 -0600

    finished productType test/methods

--- test/Bangazon.Tests/Bangazon_PaymentManagerShould.cs
+++ test/Bangazon.Tests/Bangazon_PaymentManagerShould.cs
@@ -1,29 +1,46 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 using Bangazon;
-using System.Collections.Generic;
+using Bangazon.Managers;
+using System.Linq;
+using Microsoft.Data.Sqlite;
 
-namespace Bangazon.Tests
+namespace Bangazon.Managers.Tests
 {
     public class PaymentManagerShould
     {
 
-        private PaymentType _paymentType;
+        private PaymentType paymentType;
+        private DatabaseConnection _db;
+        private  PaymentTypeManager _ptm;
+
         public PaymentManagerShould()
         {
-            //Properties of PaymentType hard coded in
-            _paymentType = new PaymentType(
-                1,
-                "Visa",
-                128798725
-            );
+
+            string prodPath = System.Environment.GetEnvironmentVariable("BANGAZON_TEST_DB");
+            // Create connection to test database and capture in DatabaseConnection variable db
+            DatabaseConnection db = new DatabaseConnection(prodPath);
+            // Set the private DatabaseConnection variable of _db to equal the DatabaseConnection of db
+            _db = db;
+            // Ensure there is a database with tables at the end of the test database connection
+            DatabaseStartup databaseStartup = new DatabaseStartup(_db);
+
+            _ptm = new PaymentTypeManager(_db);
 
         }
 
         [Fact]
         public void AddPaymentType()
         {
-            Assert.Equal(_paymentType.Id, 1);
+            PaymentType paymentType = new PaymentType();
+                paymentType.Name = "Visa";
+                paymentType.AccountNumber = 987944895;
+            var result = _ptm.AddPaymentType(paymentType);
+
+            Assert.True(result != 0);
+
+            // Assert.Equal(_paymentType.Id, 1);
         }
     }
 }
